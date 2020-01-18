35af3fcedb23143c32e93bef3b7aaf34a84b62e3
Administrator <365527950@qq.COM>
2020-01-18 21:21:12 +0800

    Revert "rabbitmq"
    This reverts commit 305b4e36

--- src/main/java/ps/Send.java
+++ src/main/java/ps/Send.java
@@ -1,5 +1,6 @@
 package ps;
 
+import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import util.ConnectionUtils;
 
@@ -12,6 +13,15 @@ public class Send {
     public static void main(String[] args) throws IOException, TimeoutException {
 
         Connection connection = ConnectionUtils.getConnection();
+        Channel channel = connection.createChannel();
+        // 声明交换机 转发器
+        channel.exchangeDeclare(EXCHANGE_NAME, "fanout");
+        // 发送消息
+        String msg = "hello ps";
+        channel.basicPublish(EXCHANGE_NAME, "", null, msg.getBytes());
+        System.out.println("Send:" + msg);
+        channel.close();
+        connection.close();
 
     }
 }
